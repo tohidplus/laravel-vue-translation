c34aa08cdbe0e0450633acbf3329d1d7da2fc06c
Andrii Bidlovskiy <angryalf@gmail.com>
2021-05-19 15:40:17 +0300

    Update Translation.js
    fix few cases when key exist in one translation but do not exist in another

--- src/resources/js/VueTranslation/Translation.js
+++ src/resources/js/VueTranslation/Translation.js
@@ -4,14 +4,16 @@ export default {
         let lang = document.documentElement.lang;
         let word = translations[lang];
         let fallback_locale = document.querySelector('meta[name="fallback_locale"]') || null;
-        
+
+        const getAltValue = (object, keys) => keys.split('.').reduce((o, k) => (o || {})[k], object);
+
         const keys = key.split('.');
         for (let i in keys) {
             try {
                 word = word[keys[i]];
                 if (word === undefined) {
                     if (fallback_locale.content){
-                        word = translations[fallback_locale.content][keys[i]];
+                        word = getAltValue(translations[fallback_locale.content], key) || key;
                     } else {
                         word = key;
                     }
